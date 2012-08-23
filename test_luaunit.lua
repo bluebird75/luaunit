8377ec35e75d1326e5260985b733ae6525b03b1f
Bluebird75 <phil@freehackers.org>
2012-08-23 23:31:56 +0200

    - improvements to assertError testing

--- test_luaunit.lua
+++ test_luaunit.lua
@@ -14,39 +14,49 @@ TestLuaUnit = {} --class
         executedTests = {}
     end
 
-
     function TestLuaUnit:test_assertError()
-        local function f() end
+        local function f( v ) 
+            v = v + 1
+        end
+        local function f_with_error(v)
+            v = v + 2
+            error('coucou')
+        end
 
-        has_error = not pcall( error, "coucou" )
-        assert( has_error == true )
-        assertError( error, "coucou" )
-        has_error = not pcall( assertError, error, "coucou" )
-        assert( has_error == false )
+        local x = 1
 
-        has_error = not pcall( f )
-        assert( has_error == false )
-        has_error = not pcall( assertError, f )
-        assert( has_error == true )
+        -- f_with_error generates an error
+        has_error = not pcall( f_with_error, x )
+        assertEquals( has_error, true )
+
+        -- f does not generate an error
+        has_error = not pcall( f, x )
+        assertEquals( has_error, false )
+
+        -- assertError is happy with f_with_error
+        assertError( f_with_error, x )
+
+        -- assertError is unhappy with f
+        has_error = not pcall( assertError, f, x )
+        assertError( has_error, true )
 
         -- multiple arguments
-        local function multif(a,b,c)
+        local function f_with_multi_arguments(a,b,c)
             if a == b and b == c then return end
             error("three arguments not equal")
         end
 
-        assertError( multif, 1, 1, 3 )
-        assertError( multif, 1, 3, 1 )
-        assertError( multif, 3, 1, 1 )
+        assertError( f_with_multi_arguments, 1, 1, 3 )
+        assertError( f_with_multi_arguments, 1, 3, 1 )
+        assertError( f_with_multi_arguments, 3, 1, 1 )
 
-        has_error = not pcall( assertError, multif, 1, 1, 1 )
-        assert( has_error == true )
+        has_error = not pcall( assertError, f_with_multi_arguments, 1, 1, 1 )
+        assertEquals( has_error, true )
     end
 
     function TestLuaUnit:test_assertEquals()
         assertEquals( 1, 1 )
-        has_error = not pcall( assertEquals, 1, 2 )
-        assert( has_error == true )
+        assertError( assertEquals, 1, 2)
     end
 
     function TestLuaUnit:Xtest_xpcall()
@@ -59,9 +69,19 @@ TestLuaUnit = {} --class
         assertEquals( prefixString( '12 ', 'ab\ncd\nde'), '12 ab\n12 cd\n12 de' )
     end
 
-    function TestLuaUnit:test_MethodsAreExecutedInRightOrder()
+    executedTests = {}
+
+    MyTestToto1 = {} --class
+        function MyTestToto1:test1() table.insert( executedTests, "MyTestToto1:test1" ) end
+        function MyTestToto1:testb() table.insert( executedTests, "MyTestToto1:testb" ) end
+        function MyTestToto1:test3() table.insert( executedTests, "MyTestToto1:test3" ) end
+        function MyTestToto1:testa() table.insert( executedTests, "MyTestToto1:testa" ) end
+        function MyTestToto1:test2() table.insert( executedTests, "MyTestToto1:test2" ) end
+
+    function TestLuaUnit:Xtest_MethodsAreExecutedInRightOrder()
         assertEquals( #executedTests, 0 )
-        LuaUnit:runTestClassByName( 'MyTestToto1' )
+        -- local runner = { output = nil }
+        -- LuaUnit.runTestClassByName( runner, 'MyTestToto1' )
         assertEquals( #executedTests, 5 )
         assertEquals( executedTests[1], "MyTestToto1:test1" )
         assertEquals( executedTests[2], "MyTestToto1:test2" )
@@ -126,15 +146,6 @@ TestLuaUnit = {} --class
 
 --[[ Class to test that tests are run in the right order ]]
 
-executedTests = {}
-
-MyTestToto1 = {} --class
-    function MyTestToto1:test1() table.insert( executedTests, "MyTestToto1:test1" ) end
-    function MyTestToto1:testb() table.insert( executedTests, "MyTestToto1:testb" ) end
-    function MyTestToto1:test3() table.insert( executedTests, "MyTestToto1:test3" ) end
-    function MyTestToto1:testa() table.insert( executedTests, "MyTestToto1:testa" ) end
-    function MyTestToto1:test2() table.insert( executedTests, "MyTestToto1:test2" ) end
-
 --[[
 TestToto2 = {} --class
     function TestToto2:test1() table.insert( executedTests, "TestToto2:test1" ) end
@@ -177,5 +188,39 @@ TestTotob = {} --class
 -- LuaUnit:run('TestLuaBinding:test_setline') -- will execute only one test
 -- LuaUnit:run('TestLuaBinding') -- will execute only one class of test
 -- LuaUnit.result.verbosity = 0
+
+function debug_print( event )
+    local info = debug.getinfo(2, 'n')
+    level = level or 0
+    if event == 'call' then
+        level = level + 1
+    end
+    indentPrefix = string.rep( '  ', level )
+    local name = info.namewhat
+    if info.namewhat ~= info.name then
+        name = name..info.name
+    end
+    print( "DEBUG: "..indentPrefix..event..' '..name )
+    if event == 'return' then
+        level = level - 1
+    end
+end
+
+-- debug.sethook( debug_print, 'cr' )
 LuaUnit:run() -- will execute all tests
 
+--[[ More tests ]]
+-- check return value of Run()
+-- check failure count and test count
+-- check that output are called with correct values
+-- check that assertions produce real errors
+-- strip luaunit stack more intelligently
+-- table assertions
+-- better verbosity support
+-- assert contains
+-- more user documentation
+-- compatibilty tests with several version of lua
+-- allow for errors in teardown and setup
+-- fix the absence of foreachi
+-- real test for wrapFunctions
+-- allow to pass real instance and real names of class and functions
