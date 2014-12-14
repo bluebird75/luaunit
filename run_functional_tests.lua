b76c69c76b1bd4cce49c6fa9eff71125b8e0f1cf
Bluebird75 <75.bluebird@gmail.com>
2014-12-14 18:02:07 -0500

    Improve run_func_tests and make it compatible with Lua 5.1 / 5.2
    - report success in exit code
    - structure to support more than one functional test
    - option to not generate the xml output to be validated

--- run_functional_tests.lua
+++ run_functional_tests.lua
@@ -4,22 +4,40 @@ function report( s )
 	print('>>>>>>> '..s )
 end
 
-function validate_junit_xml()
+function validate_junit_xml( generateJunitXml )
+	-- Set generateJunitXml to refresh XML. Default is true.
+
+	local retCode = 0
 	-- validate that junit output is a valid xml file
 	-- this assumes that xmllint is installed !
-	
+
+	if generateJunitXml == nil then
+		generateJunitXml = true
+	end
+
 	fnameJunitXml = 'output_junit.xml' -- os.tmpname()
 	fnameJunitStdout = 'junit_stdout.txt' -- os.tmpname()
-	exitSuccess, exitReason, exitCode = os.execute(string.format(
-		'lua example_with_luaunit.lua --output junit --name %s > %s', fnameJunitXml, fnameJunitStdout )	 )
+	if generateJunitXml then
+		exitSuccess, exitReason, exitCode = os.execute(string.format(
+			'lua example_with_luaunit.lua --output junit --name %s > %s', fnameJunitXml, fnameJunitStdout )	 )
+	end
 	exitSuccess, exitReason, exitCode = os.execute( string.format( 
 		'xmllint %s', fnameJunitXml ) )
-	if exitSuccess == true or (exitReason == 'exit' and exitCode == 0) then
+
+	-- Lua 5.1 : exitSuccess == 0
+	-- Lua 5.2 : exitSuccess == true and exitReason == exit and exitCode == 0
+	if exitSuccess == 0 or (exitSuccess == true and exitReason == 'exit' and exitCode == 0) then
 		report(string.format('XMLLint validation successful: file %s', fnameJunitXml) )
 	else
 		report(string.format('XMLLint reported errors : file %s', fnameJunitXml) )
+		retCode = 1
 	end
-
+	return retCode
 end
 
-validate_junit_xml()
+
+-- main section
+errorCount = 0
+errorCount = errorCount + validate_junit_xml()
+os.exit( errorCount )
+
