06c1e721e3403fdda3ed77be693b16b4d70b66f5
Bluebird75 <75.bluebird@gmail.com>
2014-12-30 23:43:50 -0500

    First attempt at validating the output of LuaUnit in TAP mode.
    - refactor a generic osExec() function
    - function to adjust the lines of a file when they match a pattern
    - add one reference output
    - functional test now validate one reference output

--- run_functional_tests.lua
+++ run_functional_tests.lua
@@ -4,10 +4,78 @@ function report( s )
     print('>>>>>>> '..s )
 end
 
+function osExec( s )
+    -- execute s with os.execute and return true if exit code is 0
+    -- false in any other conditions
+
+    -- print('osExec('..s..')')
+    exitSuccess, exitReason, exitCode = os.execute( s )
+    -- print(exitSuccess)
+    -- print(exitReason)
+    -- print(exitCode)
+
+    -- Lua 5.1 : exitSuccess == 0
+    -- Lua 5.2 : exitSuccess == true and exitReason == exit and exitCode == 0
+    if exitSuccess == 0 or (exitSuccess == true and exitReason == 'exit' and exitCode == 0) then
+        return true
+    else
+        return false
+    end
+end
+
+function adjustFile( fileOut, fileIn, pattern )
+    --[[ Adjust the content of fileOut by copying lines matching pattern from fileIn
+
+    fileIn lines are read and the first line matching pattern is analysed. The first pattern
+    capture is memorized.
+
+    fileOut lines are then read, and the first line matching pattern is modified, by applying
+    the first capture of fileIn. fileOut is then rewritten.
+    ]]
+    source = nil
+    for line in io.lines(fileIn) do
+        idxStart, idxEnd, capture = string.find( line, pattern )
+        if idxStart ~= nil then
+            source = capture
+            break
+        end
+    end
+
+    if source == nil then
+        error('No line in file '..fileIn..' matching pattern '..pattern )
+    end
+
+    -- print('Captured: '.. source )
+
+    dest = nil
+    linesOut = {}
+    for line in io.lines(fileOut) do
+        idxStart, idxEnd, capture = string.find( line, pattern )
+        if idxStart ~= nil then
+            dest = capture
+            -- print('Modifying line: '..line )
+            line = string.gsub( line, dest, source )
+            -- print('Result: '..line )
+        end
+        table.insert( linesOut, line )
+    end
+
+    if dest == nil then
+        error('No line in file '..fileOut..' matching pattern '..pattern )
+    end
+
+    f = io.open( fileOut, 'w')
+    for i,l in ipairs(linesOut) do
+        f:write( l..'\n' )
+    end
+    f:close()
+
+end
+
 function validate_junit_xml( fileToRun, fnameJunitXml, fnameJunitStdout, generateJunitXml )
     -- Set generateJunitXml to refresh XML. Default is true.
-
     local retCode = 0
+    local ret
     -- validate that junit output is a valid xml file
     -- this assumes that xmllint is installed !
 
@@ -16,21 +84,39 @@ function validate_junit_xml( fileToRun, fnameJunitXml, fnameJunitStdout, generat
     end
 
     if generateJunitXml then
-        exitSuccess, exitReason, exitCode = os.execute(string.format(
+        ret = osExec(string.format(
             'lua %s  --output junit --name %s > %s', fileToRun, fnameJunitXml, fnameJunitStdout )  )
     end
-    exitSuccess, exitReason, exitCode = os.execute( string.format( 
-        'xmllint %s', fnameJunitXml ) )
+    ret = osExec( string.format('xmllint %s', fnameJunitXml ) )
 
-    -- Lua 5.1 : exitSuccess == 0
-    -- Lua 5.2 : exitSuccess == true and exitReason == exit and exitCode == 0
-    if exitSuccess == 0 or (exitSuccess == true and exitReason == 'exit' and exitCode == 0) then
-        report(string.format('XMLLint validation successful: file %s', fnameJunitXml) )
+    if ret then
+        report(string.format('XMLLint validation ok: file %s', fnameJunitXml) )
     else
         report(string.format('XMLLint reported errors : file %s', fnameJunitXml) )
         retCode = 1
     end
     return retCode
+end
+
+function validate_tap_output( fileToRun, options, output, refOutput )
+    local ret
+    -- remove output
+    ret = osExec(string.format(
+            'lua %s  --output TAP %s > %s', fileToRun, options, output )  )
+    adjustFile( output, refOutput, '# Ran %d+ tests in (%d+.%d*).*')
+    adjustFile( output, refOutput, '# Started on (.*)')
+
+    ret = osExec( string.format(
+        -- ignore the first line that include the date, always different
+        -- ignore the last line that include the dureation, always different
+        -- NOTE: we need to find a better way to compare the last line
+        'diff -NP -u %s %s', refOutput, output ) )
+    if not ret then
+        report('Ouptut mismatch for file : '..output)
+        return 1
+    end
+    report('Output ok: '..output)
+    return 0
 end
 
 function main( )
@@ -48,8 +134,13 @@ function main( )
         'test/output_junit2.xml', 
         'test/junit_stdout2.txt' 
     )
+    errorCount = errorCount + validate_tap_output( 
+        'example_with_luaunit.lua', '',
+        'test/tapOutput1.txt',
+        'test/ref/tapOutput1.txt'
+    )
     os.exit( errorCount )
 end
 
--- main()
+main()
 
