f5d38911167af90230820e3118c315a7026eaa81
Laurent Deniau <laurent.deniau@gmail.com>
2016-07-27 15:23:59 +0200

    remove duplicated M.EPSILON (was copied, not moved)

--- luaunit.lua
+++ luaunit.lua
@@ -733,8 +733,6 @@ end
 -- Help Lua in corner cases like almostEquals(1.1, 1.0, 0.1), which by default
 -- may not work. We need to give a default margin; EPSILON defines the
 -- default value to use for this:
-M.EPSILON = 1E-12
-
 function M.almostEquals( actual, expected, margin )
     margin = margin or M.EPSILON
     if type(actual) ~= 'number' or type(expected) ~= 'number' or type(margin) ~= 'number' then
