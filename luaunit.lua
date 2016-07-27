5835be7190cbc4af26a0c7619730967b7029dcc2
Laurent Deniau <laurent.deniau@gmail.com>
2016-07-27 15:20:32 +0200

    add gards against non-number input for assertIsInf and assertIsNotInf (and variants)

--- luaunit.lua
+++ luaunit.lua
@@ -707,13 +707,13 @@ function M.assertNotIsNaN(value)
 end
 
 function M.assertIsInf(value)
-    if math.abs(value) ~= 1/0 then
+    if type(value) ~= "number" or math.abs(value) ~= 1/0 then
         failure("expected: inf, actual: " ..prettystr(value), 2)
     end
 end
 
 function M.assertNotIsInf(value)
-    if math.abs(value) == 1/0 then
+    if type(value) == "number" and math.abs(value) == 1/0 then
         failure("expected non inf value, received ±inf", 2)
     end
 end
