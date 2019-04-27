88efffe2d68a79862d9d371ceb1012bd22931d8d
Philippe F <phil@freehackers.org>
2019-04-27 14:57:27 +0200

    Display correctly errors in test_luaunit.lua when executing it

--- run_unit_tests.lua
+++ run_unit_tests.lua
@@ -1,6 +1,13 @@
 #!/usr/bin/env lua
 
-if not pcall( require, 'test.test_luaunit') then
+no_error, err_msg = pcall( require, 'test.test_luaunit')
+if not no_error then
+	if nil == err_msg:find( "module 'test.test_luaunit' not found" ) then
+		-- module found but error loading it
+		-- display the error by reproducing it
+		require('test.test_luaunit')
+	end
+
 	-- run_unit_tests shall also work when called directly from the test directory
 	require('test_luaunit')
 
