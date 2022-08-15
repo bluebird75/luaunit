60d302bc670d99cf71a2309e98f62c11aa96d38a
Philippe F <phil@freehackers.org>
2022-08-15 14:35:34 +0200

    Enable lua code coverage

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -11,4 +11,8 @@ export LUANUMBER=double
 # erase previous builds
 rm -rf $CI_WORKDIR/install
 
-bash setup_lua.sh
+source ci/setup_lua.sh
+lua -v -lluacov run_unit_tests.lua --shuffle          
+lua run_functional_tests.lua --coverage
+luacheck example_with_luaunit.lua luaunit.lua run_unit_tests.lua run_functional_tests.lua test/
+luacov-coveralls -v --include %./luaunit.lua
